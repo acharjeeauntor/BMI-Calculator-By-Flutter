05ff03d2354c0a79afc4126a08cac033cb4df19e
acharjeeauntor <acharjeeauntor@gmail.com>
2020-06-26 16:17:16 +0600

    Complete Application

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onPressed;
+
+  RoundIconButton({@required this.icon, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4CF5E),
+      elevation: 0.0,
+      constraints: BoxConstraints.tightFor(width: 46.0, height: 46.0),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.teal, scaffoldBackgroundColor: Colors.black),
+      home: InputPage(),
+    );
+  }
+}
